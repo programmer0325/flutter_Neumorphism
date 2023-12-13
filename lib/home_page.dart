39eb0ac2c3270bf6e10a9411ec52707c3fbf8795
programmer0325 <programmmer0325@gmail.com>
2023-12-13 13:43:23 +0500

    initial commit
    This is just the plain Flutter starting project

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,73 +1,69 @@
-import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
-import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 
+// gnav code
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isPressed = false;
-
   @override
   Widget build(BuildContext context) {
-    Offset distance = isPressed ? const Offset(10, 10) : const Offset(10, -5);
-    double blur = isPressed ? 5.0 : 15.0;
-    double depth = isPressed ? -10.0 : 10.0;
-    return Scaffold(
-        body: Center(
-            child: GestureDetector(
-              onTap: () => setState(() =>  isPressed = !isPressed),
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 600),
-                child: Container(
-                  width: 250,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.grey[300],
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: blur,
-                        offset: -distance,
-                        color: Colors.black.withOpacity(0.5),
-                        inset: true,
-                      ),
-                      BoxShadow(
-                        blurRadius: blur,
-                        offset: -distance,
-                        color: Colors.grey[400]!,
-                        inset: true,
-                      ),
-                      if(depth >= 0)
-                        BoxShadow(
-                          blurRadius: blur,
-                          offset: -distance,
-                          color: Colors.black.withOpacity(0.5),
-                        ),
-                      if(depth >= 0)
-                        BoxShadow(
-                          blurRadius: blur,
-                          offset: -distance,
-                          color: Colors.black.withOpacity(0.5),
-                        ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Icon(Icons.apple_rounded,
-                      size: 120,
-                      color: isPressed ? Colors.black : Colors.grey[500],
-                    ),
-                  ),
-                ),
+    return  Scaffold(
+      body: const Center(
+        child: Text("Welcome To Flutter Prog ",
+          style: TextStyle(fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.black,
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+          child: GNav(
+            backgroundColor: Colors.black,
+            rippleColor: Colors.grey,
+            hoverColor: Colors.black,
+            haptic: true,
+            tabBorderRadius: 40,
+            curve: Curves.easeOutExpo,
+            duration: const Duration(milliseconds: 900),
+            color: Colors.grey[100],
+            activeColor: Colors.white,
+            iconSize: 34,
+            tabBackgroundColor: Colors.grey.shade800,
+            padding: const EdgeInsets.all(10.0),
+            gap: 8,
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: "Home",
               ),
-            ),
-          
-
-        )
+              GButton(
+                icon: Icons.favorite_border_outlined,
+                text: "favorite",
+              ),
+              GButton(
+                icon: Icons.search,
+                text: "Search",
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: "Setting",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+
